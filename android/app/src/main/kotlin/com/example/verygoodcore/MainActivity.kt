925ce1c49232d53c183278c0243ef8e729b33366
Vasyl Koperdas <koperdasb@gmail.com>
2023-01-21 15:18:02 +0200

    init project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.sushi_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:sushi_app/app/app.dart';
+import 'package:sushi_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
